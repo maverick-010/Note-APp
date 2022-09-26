5b2bc279403acb2a2ac3ffe402f74da594ef5826
Mohd <ua1411978@gmail.com>
2022-09-26 19:44:27 +0530

    first commit

---
+++ app/src/main/java/com/maverick/notes/Note.kt
@@ -0,0 +1,15 @@
+package com.maverick.notes
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+// on below line we are specifying our table name
+@Entity(tableName = "notesTable")
+
+class Note (@ColumnInfo(name = "title")val noteTitle :String,@ColumnInfo(name = "description")val noteDescription :String,@ColumnInfo(name = "timestamp")val timeStamp :String) {
+    // on below line we are specifying our key and
+    // then auto generate as true and we are
+    // specifying its initial value as 0
+    @PrimaryKey(autoGenerate = true) var id = 0
+}
